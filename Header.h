e533393eed0ef02c93a50470e30a853838fe4a93
unknown <>
2017-05-07 22:38:16 +0800

    first commit

---
+++ Header.h
@@ -0,0 +1,14 @@
+void LOGIN();
+
+void INPUT_USERNAME();
+
+void INPUT_PASSWORD();
+
+void REGI();
+
+void REGI2();
+
+char username[] = "abcd";
+char username_s[20];
+char password[] = "1234";
+char password_s[20];
